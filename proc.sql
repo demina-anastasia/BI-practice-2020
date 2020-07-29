796edd3c71c3991afd0d0909f81eca80d79b0d52
demina-anastasia <68286251+demina-anastasia@users.noreply.github.com>
2020-07-29 16:43:29 +0400

    Update proc.sql

--- proc.sql
+++ proc.sql
@@ -2,10 +2,8 @@ CREATE OR REPLACE PROCEDURE procapp()
 LANGUAGE plpgsql
  AS $$
 	BEGIN
-        IF (TG_OP = 'UPDATE') THEN
-            INSERT INTO public.tblapp SELECT new.*, now(), NULL;
-			
-		ELSIF (TG_OP = 'INSERT') THEN
-            INSERT INTO public.tblapp SELECT new.*, now(), NULL;
-	END IF;
+        insert into public.tblapp select * from f.tblapp
+on conflict (appid) do update set appname=EXCLUDED.appname, startdate = now(), enddate = NULL;
+	
 	END; $$
+	
