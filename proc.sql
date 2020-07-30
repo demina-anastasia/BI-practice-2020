8ada91d7b8c79c0065419a5371e73c3d78c87dc1
demina-anastasia <68286251+demina-anastasia@users.noreply.github.com>
2020-07-30 20:24:30 +0400

    Update proc.sql

--- proc.sql
+++ proc.sql
@@ -1,14 +1,21 @@
-BEGIN
-       
-       update star.tblapp
-	   set appname=pa.appname, 
-	   enddate = now()
-       from star.tblapp sa
-       inner join public.tblapp pa 
-	   on sa.appid=pa.appid;
+CREATE OR REPLACE PROCEDURE star.procapp(
+	)
+LANGUAGE 'plpgsql'
+
+AS $BODY$BEGIN
+  
 	   
-	   insert into star.tblapp(appid,appname,startdate,enddate)
-	   select appid,appname,now(),null from public.tblapp pp
-	   where not exists (select 1 from star.tblapp ss where ss.appid = pp.appid and ss.enddate is null);
+	  update star.tblapp sa
+	set 
+	enddate = now()
+	from public.tblapp pa
+	where sa.appid=pa.appid and sa.enddate is null and  sa.updateon>now()-INTERVAL '1 DAY';
 	   
-	END; 
+	   insert into star.tblapp(appid,appname,startdate,enddate,updateon)
+	   select appid,appname,now(),null,now() from public.tblapp pp
+	   where not exists (select 1 from star.tblapp ss where ss.appid = pp.appid and ss.enddate is null ) and  pp.updateon is null 
+	   
+	   union all  select appid,appname,now(),null,now() from public.tblapp pp
+	   where not exists (select 1 from star.tblapp ss where ss.appid = pp.appid and ss.enddate is null ) and  pp.updateon>now()-INTERVAL '1 DAY';
+	   
+	END; $BODY$;
