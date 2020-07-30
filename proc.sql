578ad6f97ab2ceb516b16f927b42f761a58040d3
demina-anastasia <68286251+demina-anastasia@users.noreply.github.com>
2020-07-30 15:38:52 +0400

    Update proc.sql

--- proc.sql
+++ proc.sql
@@ -1,15 +1,14 @@
-CREATE OR REPLACE PROCEDURE procapp() 
-LANGUAGE plpgsql
- AS $$
 BEGIN
        
-       update pa
-	   set pa.appname=fa.appname
-	   from public.tblapp as pa
-       inner join f.tblapp as fa 
-	   on pa.appid=fa.appid;
-       
-	   insert into public.tblapp(appid,appname,startdate,enddate)
-	   select appid,appname,now(),null from f.tblapp;
+       update star.tblapp
+	   set appname=pa.appname, 
+	   enddate = now()
+       from star.tblapp sa
+       inner join public.tblapp pa 
+	   on sa.appid=pa.appid;
 	   
-	END; $$
+	   insert into star.tblapp(appid,appname,startdate,enddate)
+	   select appid,appname,now(),null from public.tblapp pp
+	   where not exists (select 1 from star.tblapp ss where ss.appid = pp.appid and ss.enddate is null);
+	   
+	END; 
