8c10a1d224da83919c853f2045c198585a9411e1
demina-anastasia <68286251+demina-anastasia@users.noreply.github.com>
2020-07-30 21:08:21 +0400

    Update proc.sql

--- proc.sql
+++ proc.sql
@@ -19,3 +19,18 @@ AS $BODY$BEGIN
 	   where not exists (select 1 from star.tblapp ss where ss.appid = pp.appid and ss.enddate is null ) and  pp.updateon>now()-INTERVAL '1 DAY';
 	   
 	END; $BODY$;
+
+ниже измененный мною вариант
+
+	BEGIN
+       
+       update pa
+	   set pa.appname=fa.appname
+	   from public.tblapp as pa
+       inner join f.tblapp as fa 
+	   on pa.appid=fa.appid;
+       
+	   insert into public.tblapp(appid,appname,startdate,enddate)
+	   select appid,appname,now(),null from f.tblapp;
+	   
+	END; 
