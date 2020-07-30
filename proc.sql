016d9ae0bc414d471314d16f509ec586edb4c678
demina-anastasia <68286251+demina-anastasia@users.noreply.github.com>
2020-07-30 10:31:21 +0400

    Update proc.sql

--- proc.sql
+++ proc.sql
@@ -1,9 +1,12 @@
-CREATE OR REPLACE PROCEDURE procapp() 
-LANGUAGE plpgsql
- AS $$
-	BEGIN
-        insert into public.tblapp select * from f.tblapp
-on conflict (appid) do update set appname=EXCLUDED.appname, startdate = now(), enddate = NULL;
-	
-	END; $$
-	
+BEGIN
+       
+       update pa
+	   set pa.appname=fa.appname
+	   from public.tblapp as pa
+       inner join f.tblapp as fa 
+	   on pa.appid=fa.appid;
+       
+	   insert into public.tblapp(appid,appname,startdate,enddate)
+	   select appid,appname,now(),null from f.tblapp;
+	   
+	END; 
